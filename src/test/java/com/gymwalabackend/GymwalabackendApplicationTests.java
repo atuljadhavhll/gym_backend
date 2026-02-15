f244455bf788ea726d27c7003773a96a75e075a6
atuljadhavhll <atuljadhavhll@outlook.com>
2026-02-15 14:55:41 +0530

    Add README.md

---
+++ src/test/java/com/gymwalabackend/GymwalabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymwalabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymwalabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
